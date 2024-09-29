8337896a64a67cda585d5f355bf1b9b01972d516
Shreyanshjanavat <155510315+Shreyanshjanavat@users.noreply.github.com>
2024-09-29 09:34:37 +0530

    Update Shop.jsx

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Hero from '../components/Hero/Hero'
 import Popular from '../components/Popular/Popular'
 import Offers from '../components/Offers/Offers'
-import New_collection from '../components/Newcollections/New_collection'
+// import New_collection from '../components/Newcollections/New_collection'
 import Newsletter from '../components/Newsletter/Newsletter'
 
 
